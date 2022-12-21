c1a2bc7746dec198197c76252976afee221c1833
Groovin Ghost <60123753+GroovinCode@users.noreply.github.com>
2022-12-21 11:58:31 -0800

    basic structure

--- magic/js/main.js
+++ magic/js/main.js
@@ -1,13 +1,17 @@
 // create elements
 const question = document.createElement('input');
 const getAnswer = document.createElement('button');
-const answer = document.createElement('image');
-question.value = 'Ask Me Anything';
-getAnswer.setAttribute('id', 'answerButton');
-getAnswer.setAttribute('type', 'button');
+const answer = document.createElement('img');
+question.id = 'askInput';
+question.type = 'text';
+question.placeholder = 'Ask Me A Question';
+getAnswer.id = 'answerButton';
+getAnswer.type = 'button';
 getAnswer.textContent = 'Get Answer';
+answer.id = 'answerOutput';
+answer.setAttribute('src', '/magic/images/magic8ball_start.png');
 
 // place elements on the page
 document.body.appendChild(question);
 document.body.appendChild(getAnswer);
-
+document.body.appendChild(answer);
